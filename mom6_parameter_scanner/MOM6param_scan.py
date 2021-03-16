e80c445029b41f7fb7dd820cc6ddd1119a55d84e
John Krasting <John.Krasting@noaa.gov>
2021-03-16 15:31:37 -0400

    Added back in html_header text

--- mom6_parameter_scanner/MOM6param_scan.py
+++ mom6_parameter_scanner/MOM6param_scan.py
@@ -28,6 +28,15 @@ def main(args, debug=False):
     if debug:
         print("main: args=", args)
 
+    html_header = """<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>MOM6 parameters</title>
+</head>
+"""
+
     P = collections.OrderedDict()
     for file in args.files:
         if args.namelist:
